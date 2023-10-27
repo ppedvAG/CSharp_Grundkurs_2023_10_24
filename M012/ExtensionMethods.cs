006cdcdb0f1618fcc9e6193fe5e9b40c0ab87abc
ppedvLukasK <lukask@ppedv.de>
2023-10-27 14:01:05 +0200

    M012

---
+++ M012/ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿namespace M012;
+
+public static class ExtensionMethods
+{
+	public static int Quersumme(this int x) //Mit this diese Methode an einen beliebigen Typen anhängen
+	{
+		return x.ToString().Sum(e => (int) char.GetNumericValue(e));
+	}
+
+	public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> x)
+	{
+		return x.OrderBy(e => Random.Shared.Next()); //Weise jedem Element der Liste eine Zufallszahl zu, und sortiere nach dieser Zufallszahl
+	}
+}
--- M012/Program.cs
+++ M012/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace M012;
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
+
+namespace M012;
 
 internal class Program
 {
@@ -68,9 +71,106 @@ internal class Program
 		//Methodenketten
 		List<Fahrzeug> bmwsNeu = fahrzeuge.Where(e => e.Marke == FahrzeugMarke.BMW).ToList();
 		#endregion
+
+		#region Linq mit Objektliste
+		//Alle Fahrzeuge finden die mehr als 200km/h Höchstgeschwindigkeit haben
+		fahrzeuge.Where(e => e.MaxV > 200); //Hier die Bedingung angeben -> Predicate
+
+		//Ergebnisse anschauen
+		//Breakpoint setzen -> Zeile ausführen -> Ausklappen, Results View ausklappen
+
+		//Alle VWs finden die mehr als 200km/h Höchstgeschwindigkeit haben
+		fahrzeuge.Where(e => e.Marke == FahrzeugMarke.VW).Where(e => e.MaxV > 200); //Linq Anweisungen verketten
+		fahrzeuge.Where(e => e.Marke == FahrzeugMarke.VW && e.MaxV > 200); //Vereinfachung vom Zeile darüber
+
+		//Die schnellsten Fahrzeuge finden
+		fahrzeuge.OrderByDescending(e => e.MaxV); //Hier wird keine Bedingung angegeben -> Selektor
+		//Bei Linq kommt immer eine neue Liste heraus
+		fahrzeuge.OrderBy(e => e.Marke).ThenByDescending(e => e.MaxV); //Sekundäre Sortierung mittels ThenBy(Descending)
+
+		//Fahren alle Fahrzeuge über 200km/h?
+		fahrzeuge.All(e => e.MaxV > 200); //Ergebnis bool
+		if (fahrzeuge.All(e => e.MaxV > 200))
+		{
+
+		}
+
+		//Fährt mindestens ein Fahrzeug über 300km/h?
+		fahrzeuge.Any(e => e.MaxV > 300); //Ergebnis bool
+		if (fahrzeuge.Any(e => e.MaxV > 300))
+		{
+
+		}
+
+		//Wieviele VWs haben wir?
+		fahrzeuge.Count(e => e.Marke == FahrzeugMarke.VW); //12 Schleifendurchläufe = 12
+		fahrzeuge.Where(e => e.Marke == FahrzeugMarke.VW).Count(); //12 Schleifendurchläufe + 4 Durchgänge = 16
+        Console.WriteLine(fahrzeuge.Count);
+
+		//Min, MinBy, Max, MaxBy, Sum, Average
+		fahrzeuge.Average(e => e.MaxV); //Berechne den Durchschnitt nur der Geschwindigkeiten
+		fahrzeuge.Sum(e => e.MaxV);
+
+		fahrzeuge.Min(e => e.MaxV); //Die kleinste Geschwindigkeit
+		fahrzeuge.MinBy(e => e.MaxV); //Das Fahrzeug mit der kleinsten Geschwindigkeit
+
+		fahrzeuge.Max(e => e.MaxV);	//Die größte Geschwindigkeit
+		fahrzeuge.MaxBy(e => e.MaxV); //Das Fahrzeug mit der größten Geschwindigkeit
+
+		//Die 3 schnellsten Fahrzeuge haben
+		fahrzeuge
+			.OrderByDescending(e => e.MaxV)
+			.Take(3);
+
+		//Skip und Take bei Webshop
+		//10 Artikel pro Seite
+
+		//Select
+		//Wandelt die Liste in eine neue Form um
+
+		//2 Anwendungsfälle
+		//1. Fall (80% der Fälle): Einzelnen Feld aus der Liste entnehmen
+		fahrzeuge.Select(e => e.Marke);
+		fahrzeuge.Select(e => e.Marke).Distinct(); //Duplikate entfernen
+
+		//2. Fall (20% der Fälle): Liste transformieren
+		//Aus einem Ordner alle Dateien einlesen ohne Pfad und Erweiterung
+		
+		//Ohne Linq
+		string[] pfade = Directory.GetFiles(@"C:\Windows");
+		List<string> dateinamen = new List<string>();
+		foreach (string pfad in pfade)
+			dateinamen.Add(Path.GetFileNameWithoutExtension(pfad));
+
+		//Mit Linq
+		List<string> dateinamenLinq = Directory
+			.GetFiles(@"C:\Windows") //Alle Pfade holen
+			.Select(e => Path.GetFileNameWithoutExtension(e)) //Wendet auf jedes Element der Liste die entsprechende Funktion an
+			.ToList(); //Liste erzeugen
+
+        Console.WriteLine(dateinamen.SequenceEqual(dateinamenLinq));
+		#endregion
+
+		#region Erweiterungsmethoden
+		int zahl = 328957;
+        Console.WriteLine(zahl.Quersumme());
+        Console.WriteLine(382843.Quersumme());
+
+		//Mische die Fahrzeugliste
+		fahrzeuge.Shuffle();
+		#endregion
+
+		//Durchschnitt berechnen
+		double summe = 0;
+		foreach (Fahrzeug f in fahrzeuge)
+			summe += f.MaxV;
+		Console.WriteLine(summe / fahrzeuge.Count);
+
+		Console.WriteLine($"Die Durchschnittsgeschwindigkeit ist {fahrzeuge.Average(e => e.MaxV)}");
 	}
 }
 
+[DebuggerDisplay("Marke: {Marke}, MaxV: {MaxV}")]
 public class Fahrzeug
 {
 	public int MaxV { get; set; }
