71aa349e62be9e08ad374ed4886dbada05c56dfe
ppedvLukasK <lukask@ppedv.de>
2023-10-27 11:58:11 +0200

    M011 & M012

---
+++ M011/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace M011;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		//Generische Typargumente (Generics)
+		//Stellen einen dynamischen Typen dar, der im C# Skript mit einem fixen Typen gesetzt werden kann
+		//Dieser Typ wird im Skript aber nur als Platzhalter für einen Typen angelegt
+		//Werden mit T abgekürzt
+
+		//List
+		//Klasse, die mehrere Werte halten (wie Array) aber flexibel ist von der Größe her (nicht wie Array)
+		//Die Klasse List hat ein Array darunter, welches mit Größe 4 startet und bei dem die Größe verdoppelt wird, wenn das 5. Element hinzugefügt wird
+		List<int> list = new List<int>(); //T ist hier int
+		list.Add(0); //T wurde hier zu int
+		list.Add(1);
+		list.Add(2);
+		list.Add(3);
+		list.Add(4); //Ab hier wird int[4] zu int[8]
+
+		List<string> str = new List<string>(); //T ist hier string
+		str.Add("a"); //T wurde hier zu string
+
+		List<Fahrzeug> fahrzeuge = new List<Fahrzeug>(); //T ist hier Fahrzeug
+		fahrzeuge.Add(new Fahrzeug());
+		fahrzeuge.Add(new Fahrzeug());
+		fahrzeuge.Add(new Fahrzeug());
+
+        Console.WriteLine(list.Count); //Gibt die Anzahl der Elemente zurück (= array.Length)
+
+		foreach (int i in list) //foreach genau wie bei Array
+            Console.WriteLine(i);
+
+		Console.WriteLine(list[0]); //Index genau wie bei Array
+
+		list.Clear(); //Entleert die Liste
+
+		list.Sort(); //Sortiert die Liste (nicht sinnvoll bei Objekt Listen)
+
+		list.Contains(3); //Ist ein Element enthalten?
+
+		//Dictionary
+		//Liste von Schlüssel-Wert Paaren
+		//Enthält Schlüssel, die alle jeweils einen Wert angehängt haben
+		//Schlüssel müssen eindeutig sein
+
+		Dictionary<string, int> einwohnerzahlen = new(); //new(): Ergänzt den Typen von Links
+		einwohnerzahlen.Add("Wien", 2_000_000);
+		einwohnerzahlen.Add("Berlin", 3_650_000);
+		einwohnerzahlen.Add("Paris", 2_160_000); //3 Schlüssel, zu jedem Schlüssel ein Wert
+		//einwohnerzahlen.Add("Paris", 2_160_000); //Nicht möglich, da Schlüssel bereits existiert
+
+		if (einwohnerzahlen.ContainsKey("Salzburg")) //Prüfe ob ein Key existiert
+			Console.WriteLine(einwohnerzahlen["Salzburg"]); //Dictionary angreifen wie bei List aber mit einem Index der dem Typ des Schlüssels entspricht
+
+        Console.WriteLine(einwohnerzahlen.Count);
+
+		//var: Der Compiler ergänzt hier den Typen automatisch anhand des Inhalts der Liste
+		//var -> Strg + .: Typ ergänzen
+		foreach (KeyValuePair<string, int> kv in einwohnerzahlen) //kv enthält Schlüssel und Wert getrennt voneinander
+		{
+            Console.WriteLine($"Die Stadt {kv.Key} hat {kv.Value} Einwohner.");
+        }
+    }
+}
+
+public class Fahrzeug { }
---
+++ M012/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace M012;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		#region Einfaches Linq
+		List<int> ints = Enumerable.Range(1, 20).ToList();
+		
+		//Finde alle geraden Zahlen
+		//Ohne Linq
+		foreach (int i in ints)
+			if (i % 2 == 0)
+				Console.WriteLine(i);
+
+		//Mit Linq
+		IEnumerable<int> x = ints.Where(e => e % 2 == 0); //Selbes Ergebnis
+		//Where Funktion macht im Hintergrund eine Schleife und eine if
+
+		//IEnumerable
+		//Nur eine Anleitung, die bei Ausiterierung die fertige Liste erzeugt
+		//Mit foreach, ToList(), ToArray(), ... können die konkreten Werte erzeugt werden
+		Enumerable.Range(1, 1_000_000_000); //1ms (nur eine Anleitung)
+		//Enumerable.Range(1, 1_000_000_000).ToList(); //7s (hier werden konkrete Werte erzeugt)
+
+        Console.WriteLine(ints.Average());
+        Console.WriteLine(ints.Sum());
+        Console.WriteLine(ints.Min());
+        Console.WriteLine(ints.Max());
+
+        Console.WriteLine(ints.First()); //1
+        Console.WriteLine(ints.FirstOrDefault()); //1
+
+        //Console.WriteLine(ints.First(e => e % 50 == 0)); //Bei First kann auch eine Bedingung angegeben werden
+        Console.WriteLine(ints.FirstOrDefault(e => e % 50 == 0));
+		//First() wirft eine Exception, wenn anhand der Bedingung kein Element gefunden wird
+		//FirstOrDefault() gibt einfach den Standardwert des Typens der Collection zurück (int: 0, string: null, bool: false, Fahrzeug: null, ...)
+		#endregion
+
+		List<Fahrzeug> fahrzeuge = new List<Fahrzeug>
+		{
+			new Fahrzeug(251, FahrzeugMarke.BMW),
+			new Fahrzeug(274, FahrzeugMarke.BMW),
+			new Fahrzeug(146, FahrzeugMarke.BMW),
+			new Fahrzeug(208, FahrzeugMarke.Audi),
+			new Fahrzeug(189, FahrzeugMarke.Audi),
+			new Fahrzeug(133, FahrzeugMarke.VW),
+			new Fahrzeug(253, FahrzeugMarke.VW),
+			new Fahrzeug(304, FahrzeugMarke.BMW),
+			new Fahrzeug(151, FahrzeugMarke.VW),
+			new Fahrzeug(250, FahrzeugMarke.VW),
+			new Fahrzeug(217, FahrzeugMarke.Audi),
+			new Fahrzeug(125, FahrzeugMarke.Audi)
+		};
+
+		#region Vergleich Linq Schreibweisen
+		//Alle BMWs finden
+		List<Fahrzeug> bmwsForEach = new();
+		foreach (Fahrzeug f in fahrzeuge)
+			if (f.Marke == FahrzeugMarke.BMW)
+				bmwsForEach.Add(f);
+
+		//Alte Linq Schreibweise, seit 2007 am Ende
+		List<Fahrzeug> bmwsLinqAlt = (from f in fahrzeuge
+									  where f.Marke == FahrzeugMarke.BMW
+									  select f).ToList();
+
+		//Methodenketten
+		List<Fahrzeug> bmwsNeu = fahrzeuge.Where(e => e.Marke == FahrzeugMarke.BMW).ToList();
+		#endregion
+	}
+}
+
+public class Fahrzeug
+{
+	public int MaxV { get; set; }
+
+	public FahrzeugMarke Marke { get; set; }
+
+	public Fahrzeug(int maxV, FahrzeugMarke marke)
+	{
+		MaxV = maxV;
+		Marke = marke;
+	}
+}
+
+public enum FahrzeugMarke { Audi, BMW, VW }
